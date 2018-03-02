58b156b49de1b6b97336a7ab15b2cde386562a10
Maddy star <maddystar@Maddys-MacBook-Air.local>
2018-03-02 17:05:21 +1300

    clicked button values push to array

--- calcjs.js
+++ calcjs.js
@@ -9,17 +9,19 @@ var work = addEventListener("onClick", print);
 
 function f1(objButton){  
     document.getElementById("screen").innerHTML = (objButton.value);
+    sum.push(objButton.value);
 }
+console.log(sum)
 
 //push value to sum array
-sum.push(work);
+//sum.push(work);
 
 //calculate values in sum array and convert to string
-for (i=0; i<sum.length; i++) {
-var ans = (sum.reduce).toString;
-}
+//for (i=0; i<sum.length; i++) {
+//var ans = (sum.reduce).toString;
+//}
 
 // prints result to screen
-function res() {
-document.getElementById("screen").innerHTML = ans;
-}
+//function res() {
+//document.getElementById("screen").innerHTML = ans;
+//}
