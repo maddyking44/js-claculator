ed654d84605dc95623b0bf2c23b076c0f727b478
Maddy star <maddystar@Maddys-MacBook-Air.local>
2018-03-02 18:00:53 +1300

    attempt at equals using reduce

--- calcjs.js
+++ calcjs.js
@@ -1,6 +1,7 @@
 
 var sum=[];
-var work=0;
+var answer=0;
+var gem=0;
 
 //For numbers, print number to screen and push value to sum array
 
@@ -9,13 +10,27 @@ function f1(objButton){
     sum.push(objButton.value);
 }
 
+
+
 //for operators, print operator to screen and push operator to sum array
 function f2(objButton) {
     document.getElementById("screen").innerHTML = (objButton.value);
-    sum.push(objButton.value);
+    sum.push(objButton.value).toString;
 }
 
- console.log(sum)
+//run equation when equals is pushed
+function f3(objButton) {
+    var answer = sum.reduce(
+            function(a, b, c){
+                return (abc)
+            });
+    document.getElementById("screen").innerHTML = (answer);
+}
+
+console.log(sum)
+
+
+
 
  //if (objButton.value = "C" || "AC") {
  //   sum=[];
