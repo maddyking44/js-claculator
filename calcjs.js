92be2f7117b4072237e4fee744cf7f292189c9cc
Maddy star <maddystar@Maddys-MacBook-Air.local>
2018-03-02 16:39:25 +1300

    html and css, blocked on js

---
+++ calcjs.js
@@ -0,0 +1,22 @@
+
+var sum=[];
+var working=0;
+var calc='';
+document.getElementById("button")
+button.addEventListener("onClick", add); 
+
+function add() {
+     var num = $(this).text();
+}
+
+sum.push(num);
+
+for (i=0; i<sum.length; i++) {
+var ans = (sum.reduce).toString;
+}
+
+
+// prints sum to results div
+function res() {
+document.getElementById(result).innerHTML = ans;
+}
